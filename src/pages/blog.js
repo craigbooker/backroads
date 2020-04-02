66a123c1d366ff2bd20996de6c403e4b39b9bba3
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-04-02 14:15:20 -0500

    Created basic project files.

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+
+const blog = () => {
+  return (
+    <div>
+      Hello from the blog page<Link to="/">back home</Link>
+    </div>
+  )
+}
+
+export default blog

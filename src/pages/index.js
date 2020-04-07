931f3eae389dd7c992eb2b7b541b1c3179effabd
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-04-07 14:40:30 -0500

    Created new about component for the index page. Created title component to be used throughout project. Adde about section to index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
-import Banner from '../components/Banner'
+import Banner from "../components/Banner"
+import About from "../components/Home/About"
 
 export default () => (
   <Layout>
     <SimpleHero>
-      <Banner title="continue exploring" info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, rem." >
-      <Link to="/tours" className="btn-white">explore tours</Link>
-      </Banner> 
-    </SimpleHero> 
+      <Banner
+        title="continue exploring"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, rem."
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+    <About />
   </Layout>
 )

051fcfb1b71416bc25c0a846ace92c071ad853a3
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-04-22 13:11:35 -0500

    Added background image plugin, created styledHero component, implemented styleHero component in home.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 const blog = () => {
   return (
     <Layout>
       Hello from the blog page<Link to="/">back home</Link>
-      <Images />
     </Layout>
   )
 }

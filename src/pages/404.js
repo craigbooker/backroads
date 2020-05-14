ffd2c418c1ea7d0e1ae6caf2e6c2f6af0db3f305
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-05-14 16:45:48 -0500

    Added SEO to each page and template pages.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/Layout"
 import styles from "../css/error.module.css"
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Banner from "../components/Banner"
+import SEO from "../components/SEO"
 
 export default function error() {
   return (
     <Layout>
+      <SEO title="404 Error" />
       <header className={styles.error}>
         <Banner title="Oops! It's a dead end.">
           <AniLink fade to="/" className="btn-white">
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,17 @@ import React from "react"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import { graphql } from "gatsby"
-import Contact from '../components/Contact/Contact'
+import Contact from "../components/Contact/Contact"
+import SEO from "../components/SEO"
 
-export default function contact({data}) {
-  return <Layout><StyledHero img={data.contactBcg.childImageSharp.fluid} /><Contact /></Layout>
+export default function contact({ data }) {
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <StyledHero img={data.contactBcg.childImageSharp.fluid} />
+      <Contact />
+    </Layout>
+  )
 }
 
 export const query = graphql`
